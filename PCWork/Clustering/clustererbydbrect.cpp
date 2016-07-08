9bcca10db6ed567226aee568c53f1812a7acb14f
Seongwon Lee <cat4113@naver.com>
2016-07-08 10:47:26 +0900

    Update clustererbydbrect.cpp

--- PCWork/Clustering/clustererbydbrect.cpp
+++ PCWork/Clustering/clustererbydbrect.cpp
@@ -9,61 +9,61 @@ ClustererByDbRect::ClustererByDbRect()
 
 void ClustererByDbRect::FindDbObjects(SharedData* shdDat, const vecAnnot& annots)
 {
-
-	qDebug() << "annots length" << annots.size();
-
-	objectArray.Allocate(IMAGE_WIDTH*IMAGE_HEIGHT);
-	objectMap = objectArray.GetArrayPtr();
-	memcpy(objectMap, shdDat->ConstObjectMap(), objectArray.ByteSize());
+    
+    qDebug() << "annots length" << annots.size();
+    
+    objectArray.Allocate(IMAGE_WIDTH*IMAGE_HEIGHT);
+    objectMap = objectArray.GetArrayPtr();
+    memcpy(objectMap, shdDat->ConstObjectMap(), objectArray.ByteSize());
     objects = *(shdDat->ConstObjects());
-
-	if(annots.size() != 0){ //if annotation exist, start clustering
-		for(const auto& anno : annots)
-		{
-			vecfixList fixList;
-			first = true; //we clustering objects to one ID(first object's ID), so I set a flag 'first'.
-			oneid = -1;
-			index = 0;
-			qDebug() << anno.category << anno.instanceID << anno.imrect;
-			for(auto& seg : objects){
-				if(DoRectsInclude(anno.imrect, seg.rect)){
+    
+    if(annots.size() != 0){ //if annotation exist, start clustering
+        for(const auto& anno : annots)
+        {
+            vecfixList fixList;
+            first = true; //we clustering objects to one ID(first object's ID), so I set a flag 'first'.
+            oneid = -1;
+            index = 0;
+            qDebug() << anno.category << anno.instanceID << anno.imrect;
+            for(auto& seg : objects){
+                if(DoRectsInclude(anno.imrect, seg.rect)){
                     MergeIncludeRect(seg);
-				}
+                }
                 else if(DoRectsOverlap(anno.imrect, seg.rect)){
                     MergeOverlapRect(anno,seg,fixList);
-				}
+                }
                 index++;
-
-			}
-
-			if(oneid != -1){
+                
+            }
+            
+            if(oneid != -1){
                 FixObjectMap(fixList);
-			}
-		}
-	}
+            }
+        }
+    }
 }
 
 const cl_int* ClustererByDbRect::GetObjectMap()
 {
-	return objectMap;
+    return objectMap;
 }
 
 const vecSegment* ClustererByDbRect::GetObjects()
 {
     return &objects;
-
+    
 }
 bool ClustererByDbRect::DoRectsInclude(const ImRect& outerRect, const ImRect& innerRect)
 {
-	if(outerRect.xl <= innerRect.xl && innerRect.xh <= outerRect.xh && outerRect.yl <= innerRect.yl && innerRect.yh <= outerRect.yh)
-		return true;
-	return false;
+    if(outerRect.xl <= innerRect.xl && innerRect.xh <= outerRect.xh && outerRect.yl <= innerRect.yl && innerRect.yh <= outerRect.yh)
+        return true;
+    return false;
 }
 void ClustererByDbRect::MergeIncludeRect(Segment& seg)
 {
     qDebug() << "Rects include" << seg.id << seg.rect;
     if(!first){
-
+        
         for(int x=seg.rect.xl;x<=seg.rect.xh;x++){
             for(int y=seg.rect.yl;y<=seg.rect.yh;y++){
                 if(objectMap[IMGIDX(y,x)] == seg.id){
@@ -82,12 +82,12 @@ void ClustererByDbRect::MergeIncludeRect(Segment& seg)
 
 bool ClustererByDbRect::DoRectsOverlap(const ImRect& firstRect, const ImRect& secondRect)
 {
-	if(firstRect.xh <= secondRect.xl || secondRect.xh <= firstRect.xl || firstRect.yh <= secondRect.yl || secondRect.yh <= firstRect.yl)
-		return false;
-	return true;
+    if(firstRect.xh <= secondRect.xl || secondRect.xh <= firstRect.xl || firstRect.yh <= secondRect.yl || secondRect.yh <= firstRect.yl)
+        return false;
+    return true;
 }
 
-void ClustererByDbRect:: MergeOverlapRect(const Annotation& anno, Segment& seg, vecfixList& fixList)
+void ClustererByDbRect::MergeOverlapRect(const Annotation& anno, Segment& seg, vecfixList& fixList)
 {
     ImRect intersectRect = OverlappingRect(anno.imrect,seg.rect);
     if(GetRectsIOU(intersectRect, seg.rect)>IOU_Threshold){
@@ -99,30 +99,30 @@ void ClustererByDbRect:: MergeOverlapRect(const Annotation& anno, Segment& seg, 
         for(int y=intersectRect.yl;y<=intersectRect.yh;y++){
             for(int x=intersectRect.xl;x<=intersectRect.xh;x++){
                 if(objectMap[IMGIDX(y,x)] == seg.id) numpt++;
-
+                
             }
         }
         if(float(numpt)/seg.numpt > numpt_Threshold){
             qDebug() << "Rects overlapped over numpt_Threshold" << seg.id << seg.rect;
             fixList.emplace_back(seg.id,intersectRect.xl,intersectRect.xh,intersectRect.yl,intersectRect.yh);
-			AbsorbPlanePart(objects[baseIndex], seg, intersectRect, numpt);
+            AbsorbPlanePart(objects[baseIndex], seg, intersectRect, numpt);
         }
     }
 }
 
 ImRect ClustererByDbRect::OverlappingRect(const ImRect& firstRect, const ImRect& secondRect)
 {
-	ImRect outRect;
-	outRect.xl = smax(firstRect.xl, secondRect.xl);
-	outRect.xh = smin(firstRect.xh, secondRect.xh);
-	outRect.yl = smax(firstRect.yl, secondRect.yl);
-	outRect.yh = smin(firstRect.yh, secondRect.yh);
-
-	outRect.xl = smax(outRect.xl-1, 0);
-	outRect.xh = smin(outRect.xh+1, IMAGE_WIDTH-1);
-	outRect.yl = smax(outRect.yl-1, 0);
-	outRect.yh = smin(outRect.yh+1, IMAGE_HEIGHT-1);
-	return outRect;
+    ImRect outRect;
+    outRect.xl = smax(firstRect.xl, secondRect.xl);
+    outRect.xh = smin(firstRect.xh, secondRect.xh);
+    outRect.yl = smax(firstRect.yl, secondRect.yl);
+    outRect.yh = smin(firstRect.yh, secondRect.yh);
+    
+    outRect.xl = smax(outRect.xl-1, 0);
+    outRect.xh = smin(outRect.xh+1, IMAGE_WIDTH-1);
+    outRect.yl = smax(outRect.yl-1, 0);
+    outRect.yh = smin(outRect.yh+1, IMAGE_HEIGHT-1);
+    return outRect;
 }
 
 
@@ -150,8 +150,8 @@ void ClustererByDbRect::AbsorbPlane(Segment& basePlane, Segment& mergedPlane)
 }
 
 void ClustererByDbRect::AbsorbPlanePart(Segment& basePlane, Segment& mergedPartPlane, ImRect& intersectRect, int numIntersectpt)
-{    
-	if(basePlane.id == mergedPlane.id)
+{
+    if(basePlane.id == mergedPlane.id)
         return;
     for(int y=intersectRect.yl; y<=intersectRect.yh; y++)
     {
@@ -162,10 +162,10 @@ void ClustererByDbRect::AbsorbPlanePart(Segment& basePlane, Segment& mergedPartP
         }
     }
     basePlane.numpt += numIntersectpt;
-	mergedPlane.numpt -= numIntersectpt;
+    mergedPlane.numpt -= numIntersectpt;
     basePlane.rect.ExpandRange(intersectRect);
-	mergedPartPlane.rect.ReduceRange(intersectRect);
-	//Imrect의 하위 함수 ReduceRange 새로 만들 것.
+    mergedPartPlane.rect.ReduceRange(intersectRect);
+    //Imrect의 하위 함수 ReduceRange 새로 만들 것.
 }
 
 void ClustererByDbRect::FixObjectMap(vecfixList& fixList)
